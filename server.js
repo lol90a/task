ede334fb7a1a8a82edd0d79ce6918379e6b07021
lol90a <65480702+lol90a@users.noreply.github.com>
2024-10-27 21:49:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const Web3 = require("web3");  // CommonJS style
+const Web3 = require("web3");  
 
 const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
